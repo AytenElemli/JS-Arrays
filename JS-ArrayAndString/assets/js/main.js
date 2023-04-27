dcd1c7ba00240e295f1b0e8be8d74d72042f0865
AytenElemli <127186240+AytenElemli@users.noreply.github.com>
2023-04-27 09:01:53 +0400

    Add files via upload

---
+++ JS-ArrayAndString/assets/js/main.js
@@ -0,0 +1,28 @@
+//Array və string metodlarından istifadə edərək array-imizdə promptdan gələn dəyərin neçə ədəd olduğunu tapan script yazın.
+// Məsələn promptdan "a" dəyəri gəlirsə tərkibində "a" olan sözlərin sayını qaytarsın.
+
+
+//1-ci hal(promptla)
+
+// var element = prompt("Element daxil edin:");
+// var value=prompt("Deyer daxil edin:");
+// var say= 0;
+//     for (let i = 0; i < element.length; i++) {
+//         if (value == element[i]) {  
+//             say++;
+//         }
+//     }
+// console.log(say);
+
+
+//2-ci hal(ozumuz array daxil ederek). promptda deyer olaraq arrayda olan sozu daxil edirik.
+
+// var value=prompt("Deyer daxil edin:");
+// var array = ["Salam", "Gülnur", "Röya", "Fatma", "Salam", "Dilbər", "Röya"];
+// var say= 0;
+//     for (let i = 0; i < array.length; i++) {
+//         if (value == array[i]){  
+//             say++;
+//         }
+//     }
+// console.log(say);
